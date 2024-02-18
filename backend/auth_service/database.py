04f3fe58996c4f6a2d17fa244253ff0b7c49ba8f
gabisteph <gabriellepiresmestrinho@gmail.com>
2024-02-18 16:29:32 -0400

    login

---
+++ backend/auth_service/database.py
@@ -0,0 +1,8 @@
+# database.py
+
+from pymongo import MongoClient
+
+# Conexão com o MongoDB
+client = MongoClient("mongodb://admin:adminpassword@mongodb:27017/")
+db = client["user_data"]  # Troque "your_database" pelo nome do seu banco de dados
+collection = db["users"]
---
+++ backend/auth_service/main.py
@@ -0,0 +1,16 @@
+# main.py
+
+from fastapi import FastAPI, HTTPException
+from .database import collection
+
+app = FastAPI()
+
+# Endpoint de login
+@app.post("/login/")
+async def login(email: str, password: str):
+    # Verificar se o usuário existe no banco de dados
+    user = collection.find_one({"email": email, "password": password})
+    if user:
+        return {"message": "Login successful"}
+    else:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
