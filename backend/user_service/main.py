6f49e5764a64ce2b5f2e31e5a33d84106b1efc65
gabisteph <gabriellepiresmestrinho@gmail.com>
2024-02-17 22:11:14 -0400

    user changes

--- backend/user_service/main.py
+++ backend/user_service/main.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 # Conexão com o MongoDB
 client = MongoClient("mongodb://admin:password123@localhost:27017/")
-db = client["user_data"]  # Troque "your_database" pelo nome do seu banco de dados
+db = client["user_data"]
 collection = db["beneficiary"]
 
 # Definição do modelo de dados
@@ -22,14 +22,15 @@ app = FastAPI()
 @app.post("/beneficiaries/")
 async def create_beneficiary(beneficiary: BeneficiaryCreate):
     # Verificar se o tipo de beneficiário é válido
-    if beneficiary.type not in ['person', 'institution']:
-        raise HTTPException(status_code=400, detail="Invalid beneficiary type")
+    if beneficiary.beneficiary_type not in ['person', 'institution']:
+        raise HTTPException(status_code=400, detail="Invalid beneficiary type. Must be 'person' or 'institution'.")
 
     # Inserir o beneficiário no banco de dados MongoDB
-    result = collection.insert_one(beneficiary.dict())
-
-    # Verificar se a inserção foi bem-sucedida
-    if result.inserted_id:
-        return {"message": "Beneficiary created successfully", "beneficiary_id": str(result.inserted_id)}
-    else:
-        raise HTTPException(status_code=500, detail="Failed to create beneficiary")
+    try:
+        result = collection.insert_one(beneficiary.dict())
+        if result.inserted_id:
+            return {"message": "Beneficiary created successfully", "beneficiary_id": str(result.inserted_id)}
+        else:
+            raise HTTPException(status_code=500, detail="Failed to create beneficiary.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
